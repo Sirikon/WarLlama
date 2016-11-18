d9ba053c7d9e9e6ab1d0594a4fad19b71f69a937
Darkatom <darkatomish@gmail.com>
2016-11-18 13:27:35 +0100

    User auth management

--- src/Alpaca/urls.py
+++ src/Alpaca/urls.py
@@ -6,6 +6,7 @@ app_name = "alpaca"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^activity/(?P<activity_id>[0-9]+)$', views.activity, name='activity'),
-    url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', views.login, name='login')
+    url(r'^signup/(?P<activity_id>[0-9]*)$', views.signup, name='signup'),
+    url(r'^login/(?P<activity_id>[0-9]*)$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout')
 ]
--- src/Alpaca/views.py
+++ src/Alpaca/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as auth_login, authenticate, logout
+from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
 from .forms import *
@@ -10,39 +10,59 @@ from .models import *
 # Create your views here.
 def index(request):
     activity_list = Activity.objects.order_by('-pub_date')
-    context = {'activity_list': activity_list }
+    context = { 'user': request.user,
+                'activity_list': activity_list }
     return render(request, 'Alpaca/index.html', context)
 
 def activity(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
     session_list = activity.session_set.order_by('-start_date')
-    context = {'activity': activity,
-               'session_list': session_list}
+    context = { 'user': request.user,
+                'activity': activity,
+                'session_list': session_list }
     return render(request, 'Alpaca/activity.html', context)
 
 
-def signup(request):
+def signup(request, activity_id):
+    if request.user.is_authenticated():
+        if activity_id == "":
+            return  HttpResponseRedirect(reverse('alpaca:index'))
+        else:
+            return  HttpResponseRedirect(reverse('alpaca:activity', kwargs={'activity_id': activity_id}))
+
     if request.method == "POST":
         form = ProfileCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
-        text = "Welcome to Alpaca!"
-        return  HttpResponseRedirect(reverse('alpaca:index'))
+        
+        if activity_id == "":
+            return  HttpResponseRedirect(reverse('alpaca:index'))
+        else:
+            return  HttpResponseRedirect(reverse('alpaca:activity', kwargs={'activity_id': activity_id}))
     else:
         context = {'form': ProfileCreationForm()}
         return render(request, 'Alpaca/signup.html', context)
 
 
-def login(request):
+def login(request, activity_id):
+    if request.user.is_authenticated():
+        if activity_id == "":
+            return  HttpResponseRedirect(reverse('alpaca:index'))
+        else:
+            return  HttpResponseRedirect(reverse('alpaca:activity', kwargs={'activity_id': activity_id}))
+
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = request.POST['username']
             password = request.POST['password']
-            access = authenticate(username=user, password=password)
+            access = auth.authenticate(username=user, password=password)
             if access is not None and access.is_active:
-                auth_login(request, access)
-                return HttpResponseRedirect(reverse('alpaca:index'))
+                auth.login(request, access)
+                if activity_id == "":
+                    return  HttpResponseRedirect(reverse('alpaca:index'))
+                else:
+                    return  HttpResponseRedirect(reverse('alpaca:activity', kwargs={'activity_id': activity_id}))
             else:
                 return render(request, 'Alpaca/not_active_user.html')
         else:
@@ -51,3 +71,8 @@ def login(request):
 
     context = { 'form': AuthenticationForm() }
     return render(request, 'Alpaca/login.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('alpaca:index'))
+    
