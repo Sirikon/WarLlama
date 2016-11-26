2e4e096d4ea140081924abfd50b6d8a4f36caeab
Carlos Fernández <sirikon2@gmail.com>
2016-11-26 02:01:49 +0100

    Added production database

--- src/WarLlama/settings.py
+++ src/WarLlama/settings.py
@@ -75,12 +75,29 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASES = {}
+
+if os.environ['ALPACA_ENVIRONMENT'] == 'PRODUCTION':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['ALPACA_DB_DATABASE'],
+            'USER': os.environ['ALPACA_DB_USERNAME'],
+            'PASSWORD': os.environ['ALPACA_DB_PASSWORD'],
+            'HOST': 'localhost',
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+
+
 
 
 # Password validation
