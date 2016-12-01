f6f1597bcffd0c34af63ca0c52365216664bbda7
Darkatom <darkatomish@gmail.com>
2016-12-01 02:17:08 +0100

    SASS reafactoring

--- src/Alpaca/views/others.py
+++ src/Alpaca/views/others.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from ..models import Activity
 
@@ -9,11 +9,19 @@ from utils import sort_activity_table
 
 
 def index(request):
+    user = request.user
+    user_filter = request.GET.get('filter')
     to_sort_column = request.GET.get('order_by')
-    sorted_column = request.GET.get('last')
+    last_column = request.GET.get('last')
     
-    context = sort_activity_table(Activity.objects, to_sort_column, sorted_column)
-    context['user'] = request.user
+    activity_list = Activity.objects
+    if user_filter == "author":
+        activity_list = activity_list.filter(author__id=user.id)
+    elif user_filter == "attendant":
+        activity_list = user.attending_activities
+
+    context = sort_activity_table(activity_list, to_sort_column, last_column)
+    context['user'] = user
 
     return render(request, 'Alpaca/index.html', context)
 
